4eac0669ab70bd5fee9a80d689152d8190b06d54
Jen <dragon967852@gmail.com>
2025-01-17 21:40:18 +0800

    Operator 1. compare 2. logical 3. bit 4. member

---
+++ Operator/Operator.py
@@ -0,0 +1,20 @@
+# 運算
+# 運算符
+a = 10
+b = 3
+print(a//b) # 取整數
+print(a ** 3)# 冪運算
+# 比較運算
+print("Hello" < "Hi") # 比較asci，只比較到第二位
+# 邏輯運算
+print(1 and 0)
+print(1 or 0)
+print(not 1)
+# 位運算符
+print(5 ^ 7)# 位異或
+print(~(-7))# 取反 # 二進制兩位 : 正負號以及一位 # 原碼+1變負數
+# 成員運算符
+print(3 in (1, 2, 3))
+print(4 not in (1, 2, 3))
+print(3 is 4)
+print(3 is not 4)
